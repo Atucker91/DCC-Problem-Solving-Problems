025f64dd6238398f0972f2ca30e4d09e62c8d0a0
Atucker91 <89651874+Atucker91@users.noreply.github.com>
2021-08-31 12:24:24 -0500

    adding completed problem three

--- app.js
+++ app.js
@@ -21,26 +21,54 @@
 
 ////////////////////// Capatilize Letter ///////////////////////////
 
-let userInput = prompt("Enter a string to be capatilized");
+// let userInput = prompt("Enter a string to be capatilized");
 
-console.log(capatilizeString(userInput));
+// console.log(capatilizeString(userInput));
 
-function capatilizeString(userString) {
+// function capatilizeString(userString) {
 
-    let newString = "";
+//     let newString = "";
+
+//     for (let i = 0; i < userString.length; i++) {
+//         if (userString[i] == userString[0]) {
+//             newString += userString[i].toUpperCase();
+//         }
+//         else if (userString[i - 1] == " ") {
+//             newString += userString[i].toUpperCase();
+//         }
+//         else {
+//             newString += userString[i];
+//         }
+//     }
+
+//     return newString;
+// }
+
+
+////////////////////// Compress String ///////////////////////////
+
+
+let inputString = "aaabbbbbccccaacccbbbaaabbbaaa";
+
+console.log(compressString(inputString));
+
+function compressString(userString) {
+
+    let compressedString = "";
+    let count = 0;
 
     for (let i = 0; i < userString.length; i++) {
-        if (userString[i] == userString[0]) {
-            newString += userString[i].toUpperCase();
+        if (userString[i] == userString[i + 1]) {
+            count++;
         }
-        else if (userString[i - 1] == " ") {
-            newString += userString[i].toUpperCase();
-        }
-        else {
-            newString += userString[i];
+        else if (userString[i] != userString[i + 1]) {
+            count++
+            compressString += count + userString[i];
+            count = 0;
         }
     }
 
-    return newString;
+    return compressString;
 }
+
 
